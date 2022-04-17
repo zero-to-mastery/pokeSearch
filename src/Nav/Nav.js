fa7a4fe9853c23dd04053a903d0d8f48125d64db
Iftee1711402 <tawsifhaque97@gmail.com>
2022-04-17 08:57:59 +0600

    refactor from class components to functional components and hooks

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react'
-import './CSS/Nav.css';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-const Navigation = (props) => {
-    return (
-        <div>
-            <Navbar expand="lg">
-                 <Navbar.Brand href="#home"><img src={require('./Logo/PokeSearch.png')}/></Navbar.Brand>
-                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Pokemon</Nav.Link>
-                    <Nav.Link href="#link">Item</Nav.Link>
-                    <Nav.Link href="#Contact">Contact</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-            </Navbar>
-        </div>
-    )
-}
+import React from "react";
+import "./CSS/Nav.css";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
+const Navigation = (props) => {
+  return (
+    <div>
+      <Navbar expand="lg">
+        <Navbar.Brand href="#home">
+          <img src={require("./Logo/PokeSearch.png")} alt="pokemon logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Pokemon</Nav.Link>
+            <Nav.Link href="#link">Item</Nav.Link>
+            <Nav.Link href="#Contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
 export default Navigation;
--- src/SearchPokemon/SearchPokemon.js
+++ src/SearchPokemon/SearchPokemon.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import './CSS/SearchPokemon.css'
-import Form from 'react-bootstrap/Form'
+import React from "react";
+import "./CSS/SearchPokemon.css";
+import Form from "react-bootstrap/Form";
+
 const SearchPokemon = (props) => {
-        return (
-            /*
-           <form onSubmit={props.onSubmit}>
-                <label>
-                    Pokemon:
-                    <input type='text' value={props.pokemon} onChange={props.onChange} />
-                    <input type='submit' value='Submit' />
-                </label>
-           </form>
-           */
-            <Form onSubmit={props.onSubmit}>
-                <Form.Label>Search Pokemon</Form.Label>
-                <Form.Control type='text' placeholder='Pikachu' value={props.pokemon} onChange={props.onChange}/>
-            </Form>
-        )
-    }
-    
+  return (
+    <Form onSubmit={props.onSubmit}>
+      <Form.Label>Search Pokemon</Form.Label>
+
+      <Form.Control
+        type="text"
+        placeholder="e.g. Pikachu"
+        value={props.pokemon}
+        onChange={props.onChange}
+      />
+    </Form>
+  );
+};
+
 export default SearchPokemon;
